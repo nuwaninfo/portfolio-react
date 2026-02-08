da8d812fe39dd1891178de42c0372f8659e6c899
Nuwan Fernando <nuwaninfo@gmail.com>
2026-02-08 20:07:15 +0200

    Done services component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import About from "./components/About/About";
 import Services from "./components/services/Services";
+import MyWork from "./components/mywork/MyWork";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Services />
+      <MyWork />
     </div>
   );
 };
---
+++ src/components/mywork/MyWork.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./mywork.css";
+
+const MyWork = () => {
+  return <div>MyWork</div>;
+};
+
+export default MyWork;
