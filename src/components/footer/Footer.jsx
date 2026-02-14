43bd41562cb507df600da716abfa3147b422df25
Nuwan Fernando <nuwaninfo@gmail.com>
2026-02-14 07:18:03 +0200

    Done footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,7 +14,18 @@ const Footer = () => {
         <div className="footer-top-right">
           <div className="footer-email-input">
             <img src={user_icon} alt="User Icon" />
+            <input type="email" placeholder="Enter your email" />
           </div>
+          <div className="footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">@2026 Nuwan's Portfolio</p>
+        <div className="footer-bottom-right">
+          <p>Term of Services</p>
+          <p>Privacy Policy</p>
+          <p>Connect with me</p>
         </div>
       </div>
     </div>
