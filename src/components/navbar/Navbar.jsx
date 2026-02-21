254febab84fef6b3ecc0ada07af4bdd12f5393f6
Nuwan Fernando <nuwaninfo@gmail.com>
2026-02-21 22:22:53 +0200

    make nav bar mobile responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo_nuwan.svg";
 import NavItem from "./NavItem";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
+
   return (
     <div className="navbar">
       <img src={logo} alt="Logo" className="logo" />
-      <ul className="nav-menu">
+      <img
+        src={menu_open}
+        onClick={openMenu}
+        alt="Menu Open"
+        className="nav-mob-open"
+      />
+      <ul ref={menuRef} className="nav-menu">
+        <img
+          src={menu_close}
+          onClick={closeMenu}
+          alt="Menu Close"
+          className="nav-mob-close"
+        />
         <NavItem
           label="Home"
           active={menu === "home"}
